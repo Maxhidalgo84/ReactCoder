7d33f4ec996a557a3a4cadf9ecf70f78947356d7
Maximo Hidalgo <maxhidalgo84@gmail.com>
2022-10-05 09:17:25 -0300

    desafio cartview

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,48 +1,48 @@
 import React from 'react'
 import { useCartContext } from '../Context/CartContext'
-import { Box, Button} from '@mui/material'
+import { Box, Button } from '@mui/material'
 import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
-import styled from '@emotion/styled' 
+import styled from '@emotion/styled'
 import { Link } from 'react-router-dom'
 import DeleteIcon from '@mui/icons-material/Delete';
 
 export const Cart = () => {
 
-  const {cart, totalPrice, totalQuantity,removeItem, reset} = useCartContext()
-  
+  const { cart, totalPrice, totalQuantity, removeItem, reset } = useCartContext()
+
   console.log(totalPrice());
 
   return (
     <>
       <H1>Carrito de Compras</H1>
-        <Stack textAlign="center" margin="10px" divider={<Divider orientation="row" flexItem />}>
-            {cart.map(prod =>
-            <Stack direction={{ xs: 'column', sm: 'row' }} 
+      <Stack textAlign="center" margin="10px" divider={<Divider orientation="row" flexItem />}>
+        {cart.map(prod =>
+          <Stack direction={{ xs: 'column', sm: 'row' }}
             justifyContent="space-between"
             alignItems="center"
             margin="10px"
-            spacing={{xs:1, sm:2, md:10}} key={prod.id}> 
-              <Image src={prod.imagen1} />
-              <Title>{prod.title}</Title>
-              <Text>Subtotal: ${prod.price*prod.quantity}</Text>
-              <Text>Cantidad: {prod.quantity}</Text>
-              <Button style={{borderRadius: 35, height:"fit-content", backgroundColor: "#424949", fontSize: "12px"}}  variant="contained" startIcon={<DeleteIcon />}
-                onClick={()=>removeItem(prod.id)}>Eliminar</Button>
-            </Stack>
-          )}
-        {cart.length === 0?
-        <DataContainer >
-        <H2>Tu carrito esta vacio</H2>
-        <Link to={`/`} style={{ textDecoration: 'none', color: "white" }}>
-        <Button variant="contained" color="primary"> Ir a Tienda</Button>
-        </Link>
-        </DataContainer> :
-        <Box sx={{ flex: 1, padding: "10px 50px", margin:"15px auto", backgroundColor:"white", maxWidth:"300px" }}>
-        <h2>Cantidad de Productos: {totalQuantity()}</h2>
-        <h2>TOTAL: ${totalPrice()}</h2>  
-        <Button variant="contained" color="error" onClick={reset}>Vaciar Carrito</Button>
-        </Box>}
+            spacing={{ xs: 1, sm: 2, md: 10 }} key={prod.id}>
+            <Image src={prod.imagen1} />
+            <Title>{prod.title}</Title>
+            <Text>Subtotal: ${prod.price * prod.quantity}</Text>
+            <Text>Cantidad: {prod.quantity}</Text>
+            <Button style={{ borderRadius: 35, height: "fit-content", backgroundColor: "#424949", fontSize: "12px" }} variant="contained" startIcon={<DeleteIcon />}
+              onClick={() => removeItem(prod.id)}>Eliminar</Button>
+          </Stack>
+        )}
+        {cart.length === 0 ?
+          <DataContainer >
+            <H2>Tu carrito esta vacio</H2>
+            <Link to={`/`} style={{ textDecoration: 'none', color: "white" }}>
+              <Button variant="contained" color="primary"> Ir a Tienda</Button>
+            </Link>
+          </DataContainer> :
+          <Box sx={{ flex: 1, padding: "10px 50px", margin: "15px auto", backgroundColor: "white", maxWidth: "300px" }}>
+            <h2>Cantidad de Productos: {totalQuantity()}</h2>
+            <h2>TOTAL: ${totalPrice()}</h2>
+            <Button variant="contained" color="error" onClick={reset}>Vaciar Carrito</Button>
+          </Box>}
       </Stack>
     </>
   )
